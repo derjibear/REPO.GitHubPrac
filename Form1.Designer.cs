aa3d0ddcd9513667ef301110821425064b65e925
derjen <derjen.frick@gmail.com>
2024-04-10 10:39:22 +0200

    added ui for form 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,317 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbx1 = new System.Windows.Forms.GroupBox();
+            this.gbx2 = new System.Windows.Forms.GroupBox();
+            this.btnSave1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.radFire1 = new System.Windows.Forms.RadioButton();
+            this.radIce1 = new System.Windows.Forms.RadioButton();
+            this.radWind1 = new System.Windows.Forms.RadioButton();
+            this.radEarth1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPlayer1Name = new System.Windows.Forms.TextBox();
+            this.txtDragon1Name = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.gbx1.SuspendLayout();
+            this.gbx2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbx1
+            // 
+            this.gbx1.Controls.Add(this.txtDragon1Name);
+            this.gbx1.Controls.Add(this.txtPlayer1Name);
+            this.gbx1.Controls.Add(this.label2);
+            this.gbx1.Controls.Add(this.label1);
+            this.gbx1.Controls.Add(this.btnSave1);
+            this.gbx1.Controls.Add(this.gbx2);
+            this.gbx1.Location = new System.Drawing.Point(43, 48);
+            this.gbx1.Name = "gbx1";
+            this.gbx1.Size = new System.Drawing.Size(254, 317);
+            this.gbx1.TabIndex = 0;
+            this.gbx1.TabStop = false;
+            this.gbx1.Text = "Player 1";
+            // 
+            // gbx2
+            // 
+            this.gbx2.Controls.Add(this.radEarth1);
+            this.gbx2.Controls.Add(this.radWind1);
+            this.gbx2.Controls.Add(this.radIce1);
+            this.gbx2.Controls.Add(this.radFire1);
+            this.gbx2.Location = new System.Drawing.Point(11, 143);
+            this.gbx2.Name = "gbx2";
+            this.gbx2.Size = new System.Drawing.Size(232, 130);
+            this.gbx2.TabIndex = 0;
+            this.gbx2.TabStop = false;
+            this.gbx2.Text = "Dragons ";
+            // 
+            // btnSave1
+            // 
+            this.btnSave1.Location = new System.Drawing.Point(11, 279);
+            this.btnSave1.Name = "btnSave1";
+            this.btnSave1.Size = new System.Drawing.Size(236, 38);
+            this.btnSave1.TabIndex = 1;
+            this.btnSave1.Text = "Save";
+            this.btnSave1.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(43, 372);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(537, 66);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // radFire1
+            // 
+            this.radFire1.AutoSize = true;
+            this.radFire1.Location = new System.Drawing.Point(15, 19);
+            this.radFire1.Name = "radFire1";
+            this.radFire1.Size = new System.Drawing.Size(80, 17);
+            this.radFire1.TabIndex = 0;
+            this.radFire1.TabStop = true;
+            this.radFire1.Text = "Fire Dragon";
+            this.radFire1.UseVisualStyleBackColor = true;
+            // 
+            // radIce1
+            // 
+            this.radIce1.AutoSize = true;
+            this.radIce1.Location = new System.Drawing.Point(15, 42);
+            this.radIce1.Name = "radIce1";
+            this.radIce1.Size = new System.Drawing.Size(76, 17);
+            this.radIce1.TabIndex = 1;
+            this.radIce1.TabStop = true;
+            this.radIce1.Text = "Ice dragon";
+            this.radIce1.UseVisualStyleBackColor = true;
+            // 
+            // radWind1
+            // 
+            this.radWind1.AutoSize = true;
+            this.radWind1.Location = new System.Drawing.Point(15, 65);
+            this.radWind1.Name = "radWind1";
+            this.radWind1.Size = new System.Drawing.Size(88, 17);
+            this.radWind1.TabIndex = 2;
+            this.radWind1.TabStop = true;
+            this.radWind1.Text = "Wind Dragon";
+            this.radWind1.UseVisualStyleBackColor = true;
+            // 
+            // radEarth1
+            // 
+            this.radEarth1.AutoSize = true;
+            this.radEarth1.Location = new System.Drawing.Point(15, 88);
+            this.radEarth1.Name = "radEarth1";
+            this.radEarth1.Size = new System.Drawing.Size(88, 17);
+            this.radEarth1.TabIndex = 3;
+            this.radEarth1.TabStop = true;
+            this.radEarth1.Text = "Earth Dragon";
+            this.radEarth1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Player Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dragon Name:";
+            // 
+            // txtPlayer1Name
+            // 
+            this.txtPlayer1Name.Location = new System.Drawing.Point(97, 41);
+            this.txtPlayer1Name.Name = "txtPlayer1Name";
+            this.txtPlayer1Name.Size = new System.Drawing.Size(146, 20);
+            this.txtPlayer1Name.TabIndex = 4;
+            // 
+            // txtDragon1Name
+            // 
+            this.txtDragon1Name.Location = new System.Drawing.Point(99, 79);
+            this.txtDragon1Name.Name = "txtDragon1Name";
+            this.txtDragon1Name.Size = new System.Drawing.Size(144, 20);
+            this.txtDragon1Name.TabIndex = 5;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.groupBox4);
+            this.groupBox3.Location = new System.Drawing.Point(326, 48);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(254, 317);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Player 2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(99, 79);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(144, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(97, 41);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(146, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Dragon Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Player Name:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(236, 38);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.radioButton5);
+            this.groupBox4.Controls.Add(this.radioButton6);
+            this.groupBox4.Controls.Add(this.radioButton7);
+            this.groupBox4.Controls.Add(this.radioButton8);
+            this.groupBox4.Location = new System.Drawing.Point(11, 143);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(232, 130);
+            this.groupBox4.TabIndex = 0;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Dragons ";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(15, 88);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(88, 17);
+            this.radioButton5.TabIndex = 3;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Earth Dragon";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(15, 65);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(88, 17);
+            this.radioButton6.TabIndex = 2;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Wind Dragon";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(15, 42);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(76, 17);
+            this.radioButton7.TabIndex = 1;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Ice dragon";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(15, 19);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(80, 17);
+            this.radioButton8.TabIndex = 0;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Fire Dragon";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.gbx1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.gbx1.ResumeLayout(false);
+            this.gbx1.PerformLayout();
+            this.gbx2.ResumeLayout(false);
+            this.gbx2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbx1;
+        private System.Windows.Forms.Button btnSave1;
+        private System.Windows.Forms.GroupBox gbx2;
+        private System.Windows.Forms.RadioButton radIce1;
+        private System.Windows.Forms.RadioButton radFire1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtDragon1Name;
+        private System.Windows.Forms.TextBox txtPlayer1Name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radEarth1;
+        private System.Windows.Forms.RadioButton radWind1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
     }
 }
 
